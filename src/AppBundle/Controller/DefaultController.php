4f8570b619fcf02477d3dc167986a90b3b90954f
Tee Tanawatanakul <wallsfantasy@gmail.com>
2016-11-16 23:19:13 +0700

    Create method to make search

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +19,23 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @Route("/search", name="search")
+     */
+    public function search(Request $request)
+    {
+        $city = $request->query->get('city');
+
+        try {
+            $json = $this->get('app.service.search_city_tweet')->search($city);
+        } catch (\Exception $e) {
+            return new JsonResponse(['success' => false], 400);
+        }
+
+        return new JsonResponse($json, 200, [], true);
     }
 }
